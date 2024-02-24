eaf19b3cdcdb6b9277d75f680e3c666679c64667
Bittu7870 <dev.bittu96@gmail.com>
2024-02-24 22:11:30 +0530

    update search header component

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SearchBox = () => {
+  return <>SearchBox</>;
+};
+
+export default SearchBox;
--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,11 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import Logo from "../../public/googlelogo.png";
+import SearchBox from "./SearchBox";
+import { RiSettings3Line } from "react-icons/ri";
+import { TbGridDots } from "react-icons/tb";
 
 const SearchHeader = () => {
-  return <>
-    <div>
-        
-    </div>
-  </>;
+  return (
+    <header className="sticky top-0 bg-white ">
+      <div className="flex items-center justify-between p-6 w-full">
+        <Link href="/">
+          <Image
+            src={Logo}
+            alt="google-logo"
+            width={120}
+            height={40}
+            //   style={{ width: "auto" }}
+            priority
+          />
+        </Link>
+        <div className="flex-1">
+          <SearchBox />
+        </div>
+        <div className="hidden md:inline-flex space-x-2">
+          <RiSettings3Line className="bg-transparent hover:bg-gray-200 text-4xl p-2 rounded-full cursor-pointer " />
+          <TbGridDots className="bg-transparent hover:bg-gray-200 text-4xl p-2 rounded-full cursor-pointer " />
+        </div>
+        <button className="bg-blue-500 text-white px-5 py-2 rounded-md hover:brightness-105 hover:shadow-md font-medium transition-shadow ml-2 ">
+          SignUp
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default SearchHeader;
