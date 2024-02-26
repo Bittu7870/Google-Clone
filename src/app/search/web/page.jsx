d1a8c70026b6fd6e6f92641bfc3f9a87cf5cf307
Bittu7870 <dev.bittu96@gmail.com>
2024-02-26 11:40:38 +0530

    update web search page

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,36 @@
+import Link from "next/link";
 import React from "react";
 
-const WebSearchPage = () => {
-  return <div >Web WebSearchPage</div>;
+const WebSearchPage = async ({ searchParams }) => {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.q}`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await response.json();
+  const result = data.items;
+  if (!result) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-4xl mb-4">
+          No results found for "{searchParams.q}"
+        </h1>
+        <p className="text-lg">
+          Please try searching for something else or go back to{" "}
+          <Link href="/" className="text-blue-500 ">
+            the homepage
+          </Link>
+        </p>
+      </div>
+    );
+  }
+  return (
+    <div>
+      {result &&
+        result.map((item, index) => <div key={index}>{item.title}</div>)}
+    </div>
+  );
 };
 
 export default WebSearchPage;
